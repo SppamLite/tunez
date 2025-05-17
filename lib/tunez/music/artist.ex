8cb6ca57d1ac5ed3040e36a3a394e1a6ceb4f318
Sppam Litte <sppamlitte@gmail.com>
2025-05-16 22:23:16 -0400

    add album

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -7,8 +7,15 @@ defmodule Tunez.Music.Artist do
   end
 
   actions do
-    defaults [:create, :read, :update, :destroy]
+    defaults [:create, :read, :destroy]
     default_accept [:name, :biography]
+
+    update :update do
+      require_atomic? false
+      accept [:name, :biography]
+
+      change Tunez.Music.Changes.UpdatePreviousNames, where: [changing(:name)]
+    end
   end
 
   attributes do
@@ -22,5 +29,15 @@ defmodule Tunez.Music.Artist do
 
     create_timestamp :inserted_at
     update_timestamp :updated_at
+
+    attribute :previous_names, {:array, :string} do
+      default []
+    end
+  end
+
+  relationships do
+    has_many :albums, Tunez.Music.Album do
+      sort year_released: :desc
+    end
   end
 end
